052b1377fdcca5862fdad3a7f6674733ee78358e
Lubo <l1u9b9o0@gmail.com>
2014-06-04 12:00:17 +0200

    Zamiana na Drawable
    Zamiana grafiki w Cwiczenie z byte[] na Drawable

--- Trainer/app/src/main/java/modeleDanych/Cwiczenie.java
+++ Trainer/app/src/main/java/modeleDanych/Cwiczenie.java
@@ -1,14 +1,16 @@
 package modeleDanych;
 
+import android.graphics.drawable.Drawable;
+
 /**
  * Created by Lubo on 03.06.14.
  */
 public class Cwiczenie {
     int mCzasTrwania;
     String mNazwa;
-    byte[] mGrafika;
+    Drawable mGrafika;
 
-    public Cwiczenie(int czasTrwania, String nazwa, byte[] grafika) {
+    public Cwiczenie(int czasTrwania, String nazwa, Drawable grafika) {
         mCzasTrwania = czasTrwania;
         mNazwa = nazwa;
         mGrafika = grafika;
